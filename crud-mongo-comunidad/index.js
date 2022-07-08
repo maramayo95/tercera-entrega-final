46d7cea0195f4a4dc195ea913914d4c04a129b6e
Matias <matiasaramayo@hotmail.com>
2022-07-07 23:46:01 -0300

    .

--- crud-mongo-comunidad/index.js
+++ crud-mongo-comunidad/index.js
@@ -1,9 +1,5 @@
 const mongoose = require('mongoose');
 const app = require('./app/app')
-
-
-// const sendSMS = require('./app/utils/twilioSms');
-// const sendWhatsapp = require('./app/utils/twilioWhatsapp')
 require('dotenv').config()
 
 
@@ -12,8 +8,6 @@ const urlBase = process.env.DB
 
 if (process.env.AMBIENTE == 'mongo') mongoose.connect(urlBase)
 
-// sendSMS()
-
-// sendWhatsapp()
+console.log(PORT, 'Este es el puerto')
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`))
